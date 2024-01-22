3665070aed7f9808d181aa9dea8600cf7d1dc35f
Mohit Soni <sonimohit481@gmail.com>
2024-01-22 16:53:02 +0530

    whole app together

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
+import { useTodo } from "./context/TodoContext";
+
 function App() {
+  const { todos } = useTodo();
   return (
-    <div className="bg-[#3cb186] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+    <div className="bg-[#403cb1] min-h-screen py-8">
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white bg-green-500">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
           Manage Your Todos
         </h1>
-        <div className="mb-4">{/* Todo form goes here */}</div>
+        <div className="mb-4">
+          <TodoForm />
+        </div>
         <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+          {todos.map((todo) => (
+            <div key={todo.id} className="w-full">
+              <TodoItem todo={todo} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
