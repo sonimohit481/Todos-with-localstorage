bd8683fef196c962313d4b7e8e56cbbfaf9a7282
Mohit Soni <sonimohit481@gmail.com>
2024-01-22 13:10:57 +0530

    Context updated

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,21 +1,69 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+export interface TodoProps {
+  id: number;
+  todo: string;
+  completed: boolean;
+}
+export interface TodoContextProps {
+  todos: TodoProps[];
+  addTodo: (todo: TodoProps) => void;
+  updateTodo: (id: number, updatedTodo: TodoProps) => void;
+  deleteTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
 
-export const TodoContext = createContext({
-  todos: [
+export const TodoContext = createContext<TodoContextProps | undefined>(
+  undefined
+);
+
+export const useTodo = () => {
+  const context = useContext(TodoContext);
+  if (!context) {
+    throw new Error("useTodo must be used within a TodoProvider");
+  }
+  return context;
+};
+
+export const TodoProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [todos, setTodos] = useState<TodoProps[]>([
     {
       id: 1,
       todo: "Todos message",
       completed: false,
     },
-  ],
-  addTodo: (todo: string) => {},
-  updateTodo: (id: number, todo: string) => {},
-  deleteTodo: (id: number) => {},
-  toggleTodo: (id: number) => {},
-});
+  ]);
 
-export const useTodo = () => {
-  return useContext(TodoContext);
+  const addTodo = (todo: TodoProps) => {
+    setTodos((prev) => [{ ...todo, id: Date.now() }, ...prev]);
+  };
+
+  const updateTodo = (id: number, updatedTodo: TodoProps) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? updatedTodo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ todos, addTodo, updateTodo, toggleTodo, deleteTodo }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
 };
-
-export const TodoProvider = TodoContext.Provider;
